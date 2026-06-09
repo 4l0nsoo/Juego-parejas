fbd8d5ccc793067a8ef43deb9404a9fd047c1409
4l0nsoo <di4zalonsoj@gmail.com>
2026-06-09 18:44:11 -0500

    mejoramos diseño

--- main.py
+++ main.py
@@ -12,29 +12,32 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("900x900")
 
-cardWidth = 200
+cardWidth = 120
 cardHeight = cardWidth * 7 / 5
 
 dificultad = ""
 
+def menu():
+    boton_iniciar = tk.Button(width=10,text="Iniciar Juego",justify="center", pady=10,command=generarTablero)
+    boton_iniciar.pack()
+
 #necesita recibir el tipo de dificultad seleccionada desde el menú 
 #usar for dentro de for para hacer la rejilla, for padre seria para filas y el de dentro para columnas
-def generarTablero(difiultad):
-    if dificultad == "facil":
-        print("se selecciono dificultad facil")
+def generarTablero():
+    frame_grid = tk.Frame(root)
+    frame_grid.pack(pady=(100, 0))
+    for columna in range(0,4):
+        # fila_carta=[]
+        for fila in range(0,4):
+            #card = tk.Button(frame_grid,image=img1,bg="#3541b0",activebackground="#262e82",fg="white",font=("Arial",24,"bold"), width=cardWidth, height=cardHeight)
+            card = tk.Label(frame_grid,image=img1,bg="white",highlightbackground="#3541b0",highlightthickness=3,fg="white",font=("Arial",24,"bold"), width=cardWidth, height=cardHeight)
+            card.grid(row=fila, column=columna, padx=30, pady=10, )
+            # fila_carta.append(card)
+                
     
-    
 
-img1 = tk.PhotoImage(file="./recursos/estrella.png")
 
-card1 = tk.Label(root, image=img1, borderwidth=3, relief="solid", width=cardWidth, height=cardHeight)
-card2 = tk.Label(root, image=img1, borderwidth=3, relief="solid", width=cardWidth, height=cardHeight)
-card3 = tk.Label(root, image=img1, borderwidth=3, relief="solid", width=cardWidth, height=cardHeight)
-card4 = tk.Label(root, image=img1, borderwidth=3, relief="solid", width=cardWidth, height=cardHeight)
-card1.grid(row=0, column=0, padx=10, pady=10)
-card2.grid(row=0, column=1, padx=10, pady=10)
-card3.grid(row=0, column=2, padx=10, pady=10)
-card4.grid(row=1, column=0, padx=10, pady=10)
-
+img1 = tk.PhotoImage(file="./recursos/hamburguesa.png")
+menu()
 
 root.mainloop()
